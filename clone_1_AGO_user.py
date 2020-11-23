ddfa59294cb222cd63aa74288ced37f4bb482387
Tom Baker <tbaker@ku.edu>
2020-11-23 14:20:41 -0600

    Add files via upload

---
+++ clone_1_AGO_user.py
@@ -0,0 +1,33 @@
+from arcgis import GIS
+
+# Use admin logins on both organizations
+source = GIS("https://xxxxxx.maps.arcgis.com", "", "")
+target = GIS("https://xxxxxx.maps.arcgis.com", "", "")
+
+itemTypes=''
+sourceUser='xxxx'
+targetUser='xxxx'
+
+sourceUserObj = source.users.get(username=sourceUser)
+for folder in sourceUserObj.folders:
+    print(' ')
+    print(folder['title'], " -- ", folder['id'], "  ")
+    # clone folder name
+    target.content.create_folder(folder=folder['title'], owner=targetUser)
+    #targetUserObj.items(folder['title'])
+    itemsInFolders = sourceUserObj.items(folder=folder['title'])
+    #print(len(itemsInFolders))
+    for i in itemsInFolders:
+        print("     " +i.title + i.type)
+        clItem = source.content.get(i.id)
+        try:
+            target.content.clone_items([clItem],
+                                    folder=i.title,
+                                    owner=targetUser
+                                    )
+        except:
+            print("     ---" , 'Error on cloning: ', i.title)
+            pass
+
+print(' ')
+print('Script complete.')
